f94050784c6cadc29e1229a271d37c7b93470330
jshuk-7 <Jessieshukeireh2001@gmail.com>
2023-05-23 20:49:02 -0400

    added dyn_string and dyn_array as submodules

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 
 set SRC="tors/src/tors.c"
-set INCLUDE_DIR="vendor"
+set DYN_STRING_INCLUDE="vendor/dyn_string/dyn_string/include"
+set DYN_ARRAY_INCLUDE="vendor/dyn_array/dyn_array/include"
 
-gcc %SRC% -o "build/tors.exe" -I %INCLUDE_DIR%
+gcc %SRC% -o "build/tors.exe" -I %DYN_STRING_INCLUDE% -I %DYN_ARRAY_INCLUDE%
 
 pause
--- tors/src/tors.c
+++ tors/src/tors.c
@@ -1,6 +1,8 @@
 #define TORS_IMPLEMENTATION
 #include "tors.h"
 
+#include <stdio.h>
+
 int main(void) {
 	return 0;
 }
