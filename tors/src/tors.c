a36cb551c75b25be4c3639411b96f1d8cafefe6e
jshuk-7 <Jessieshukeireh2001@gmail.com>
2023-05-24 14:55:07 -0400

    added tokenizer mvp

--- tors/src/tors.c
+++ tors/src/tors.c
@@ -4,5 +4,15 @@
 #include <stdio.h>
 
 int main(void) {
+	DString input = dstring_create("This is the beginning!");
+	DArray* tokens = tors_tokenize_string(&input);
+
+	for (size_t i = 0; i < tokens->buf_size; i++) {
+		Token token = DARRAY_ELEMENT(tokens, Token, i);
+		printf("%zu: %zu\n", i, token.ascii_sum);
+	}
+
+	dstring_destroy(&input);
+	darray_destroy(tokens);
 	return 0;
 }
--- tors/src/tors.h
+++ tors/src/tors.h
@@ -25,10 +25,99 @@
  * SOFTWARE.
 */
 
+#include <stdint.h>
+#include <ctype.h>
+
+#ifndef TORS_MALLOC
+#include <stdlib.h>
+#define TORS_MALLOC malloc
+#endif
+
+#ifndef TORS_REALLOC
+#include <stdlib.h>
+#define TORS_REALLOC realloc
+#endif
+
+#ifndef TORS_FREE
+#include <stdlib.h>
+#define TORS_FREE free
+#endif
+
+#ifndef TORS_ASSERT
+#include <assert.h>
+#define TORS_ASSERT assert
+#endif
+
+#define DYN_STRING_IMPLEMENTATION
+#include "dyn_string.h"
+
+#define DYN_ARRAY_IMPLEMENTATION
+#include "dyn_array.h"
+
+typedef enum
+{
+    TT_WORD,
+    TT_PUNCTUATION,
+    TT_ARITHMETIC,
+} TokenType;
+
+typedef struct
+{
+    TokenType type;
+    size_t ascii_sum;
+} Token;
+
+/// @brief Converts the input to a stream of tokens. To free the result use darray_destroy.
+/// @param input the input string
+/// @return a dynamic array of tokens
+DArray* tors_tokenize_string(const DString* input);
+
 #endif
 
 #ifdef TORS_IMPLEMENTATION
 
+static void add_token(DArray* tokens, const Token* token) {
+    darray_push_back(tokens, sizeof(Token), token);
+}
 
+DArray* tors_tokenize_string(const DString* input)
+{
+    TORS_ASSERT(input->data);
+    DArray* tokens = darray_create(sizeof(Token));
+    size_t start = 0;
+    size_t cursor = 0;
+
+    while (cursor < input->size) {
+        char c = dstring_get_char(input, cursor);
+        start = cursor;
+
+        if (isalpha(c)) {
+            while (isalpha(c)) {
+                c = dstring_get_char(input, ++cursor);
+            }
+            size_t count = cursor - start;
+            DString lexeme = dstring_substring(input, start, count);
+            size_t sum = 0;
+            for (size_t i = 0; i < lexeme.size; i++) {
+                sum += (size_t)(uint32_t)dstring_get_char(&lexeme, i);
+            }
+            Token token = { .type = TT_WORD, .ascii_sum = sum };
+            add_token(tokens, &token);
+        }
+
+        if (isspace(c)) {
+            cursor++;
+        }
+
+        if (c == '!') {
+            size_t sum = (size_t)(uint32_t)c;
+            Token token = { .type = TT_PUNCTUATION, .ascii_sum = sum };
+            add_token(tokens, &token);
+            cursor++;
+        }
+    }
+
+    return tokens;
+}
 
 #endif
